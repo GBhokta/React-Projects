2262923642f97a00b45dd9608339a3724650fc67
GBhokta <georgebhokta@gmail.com>
2026-01-17 19:13:24 +0530

    Initial Commit

---
+++ personal-dashboard/src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <h1>Setup Complete</h1>
+          <p>If this looks styled, your design system is working.</p>
+        </div>
+      </div>
+      <div className="section">
+        <div className="container">
+          <h2>Next Steps</h2>
+          <ol>
+            <li>Delete the contents of this App.jsx file.</li>
+            <li>Start building your personal dashboard!</li>
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+}
